23366f457fa97e7b0bf79c98d147654edb8972e5
TranDHung <tranduchung0610@gmail.com>
2023-09-26 15:00:25 +0700

    add

--- backend/controllers/customer/getAll.js
+++ backend/controllers/customer/getAll.js
@@ -3,7 +3,7 @@ import Customer from '../../models/customer.js';
 export const getAll = async (req, res, next) => {
     try {
         await Customer.createIndexes({_id: 1});
-        const fetchCustomer = await Customer.find({}, {_id: 1, name: 1, phonenum: 1, bankAccountNo: 1, bankAccountName: 1, bankName: 1, discount: 1, del: 1});
+        const fetchCustomer = await Customer.find({del: 0}, {_id: 1, name: 1, phonenum: 1, bankAccountNo: 1, bankAccountName: 1, bankName: 1, discount: 1, del: 1});
         return res.json({ 
             status: 200,
             message: "Thành công.",
