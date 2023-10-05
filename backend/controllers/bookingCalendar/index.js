5ac06775da8750244fc77e06c6d43761a366b8a3
TranDHung <tranduchung0610@gmail.com>
2023-10-05 16:17:59 +0700

    add

--- backend/controllers/bookingCalendar/index.js
+++ backend/controllers/bookingCalendar/index.js
@@ -1,5 +1,6 @@
 import { getAll } from "./getAllByWeek.js";
 import { create } from "./create.js";
 import { remove } from "./delete.js";
+import { exportB } from "./export.js";
 
-export default { getAll, create, remove }
+export default { getAll, create, remove, exportB }
--- backend/routes/bookingCalendar.js
+++ backend/routes/bookingCalendar.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post("/get-all",          func.getAll);
 router.post("/create",           func.create);
 router.post("/delete",           func.remove);
+router.post("/export",           func.exportB);
 
 export default router;
