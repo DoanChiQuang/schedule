17c919defe9999ba27703ee1589c6009ad617ece
TranDHung <tranduchung0610@gmail.com>
2023-10-06 13:34:37 +0700

    add

--- backend/controllers/bookingCalendar/export.js
+++ backend/controllers/bookingCalendar/export.js
@@ -71,7 +71,7 @@ export const exportB = async (req, res, next) => {
                     //     bonus: fetchAllCalendars[j].bonus,
                     //     total: fetchAllCalendars[j].total
                     // };
-                    bookingCal.push(fetchAllCalendars[j].updatedAt);
+                    bookingCal.push(formatDateToString(fetchAllCalendars[j].updatedAt));
                     bookingCal.push(fetchAllCalendars[j].customerName)
                     bookingCal.push(details.toString())
                     bookingCal.push(fetchAllCalendars[j].note)
@@ -138,3 +138,14 @@ export const exportB = async (req, res, next) => {
         next(error);
     }
 }
+
+function formatDateToString(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  }
