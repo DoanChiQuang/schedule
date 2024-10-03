738be541e6e7983060f8f5ea2fbfec509771dfa9
DoanChiQuang <chiquang127@gmail.com>
2024-10-03 14:57:53 +0700

    Optimized middleware error handling for status code 404, 50x

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,29 +1,22 @@
 import express from 'express'
 import AuthRoute from './auth.js'
+import { auth } from '../middlewares/auth.js'
 
 const startup = (app) => {
     app.use(express.json())
 
     app.use('/api', AuthRoute)
 
-    app.use((err, req, res, next) => {
-        const message = {}
-        // Success Handler
-        if(!err) {
-            message.success = true;
-            message.status = req.resStatus || 200;
-            message.message = req.resMessage || "Successfully";
-            message.data = req.resObject;        
-        }
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500)
+        res.send({
+            msg: err.message || 'Something goes wrong. Please contact us',
+        })
+    })
 
-        // Error Handler
-        message.success = false;
-        message.status = err.statusCode || 500;
-        message.message = err.message || 'Internal Server Error';
-        message.data = {};        
-
-        res.status(message.status).send(message);
-        return next();
+    app.use((req, res) => {
+        res.status(404)
+        res.send({ msg: "Sory, can't find that" })
     })
 }
 
