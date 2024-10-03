1681661d262b72cda5d077e1611e90acddde6e70
DoanChiQuang <chiquang127@gmail.com>
2024-10-03 15:00:26 +0700

    Implement JWT sign and verify function

---
+++ server/app/lib/jwt.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+
+const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY
+const TOKEN_LIFE = process.env.TOEKN_LIFE
+
+export const sign = (payload) => {
+    try {
+        return jwt.sign(payload, TOKEN_SECRET_KEY, {
+            expiresIn: TOKEN_LIFE,
+        })
+    } catch (error) {
+        console.error(`[Error]: ${error}`)
+        return null
+    }
+}
+
+export const verify = (token) => {
+    try {
+        return jwt.verify(token, TOKEN_SECRET_KEY)
+    } catch (error) {
+        console.error(`[Error]: ${error}`)
+        return null
+    }
+}
