530446c9d2b04e1769593e8897127cfc38bc535b
DoanChiQuang <chiquang127@gmail.com>
2024-10-04 09:47:09 +0700

    Update to use token by cookie

--- server/app/middlewares/auth.js
+++ server/app/middlewares/auth.js
@@ -1,15 +1,15 @@
-import { verify } from "../lib/jwt.js";
+import { verify } from '../lib/jwt.js'
 
 export const auth = async (req, res, next) => {
-    const tokenHeader = req.headers.authorization;
-    if(!tokenHeader) {
+    const tokenCookie = req.cookies.access_token
+    if (!tokenCookie) {
         res.status(401).send({ msg: 'Request missing Authorization Data' })
         return
     }
 
-    const token = tokenHeader.split(' ')[1]
+    const token = tokenCookie.split(' ')[1]
     const verified = verify(token)
-    if(!verified) {
+    if (!verified) {
         res.status(401).send({ msg: 'Unauthorized' })
         return
     }
