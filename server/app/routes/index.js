f9cbc1c2718f8434a9fc9bcc96fe94ce706fffda
DoanChiQuang <chiquang127@gmail.com>
2024-10-04 09:48:05 +0700

    Config Server can read cookie and use cors

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,11 +1,25 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import AuthRoute from './auth.js'
 import { auth } from '../middlewares/auth.js'
 
 const startup = (app) => {
+    app.use(cors({
+        origin: 'http://localhost:3000',
+        credentials: true
+    }))
+    app.use(cookieParser())
     app.use(express.json())
 
     app.use('/api', AuthRoute)
+
+    // Test
+    app.use('/api/users', auth, (req, res, next) => {
+        res.send({
+            msg: 'User List'
+        })
+    });
 
     app.use(function (err, req, res, next) {
         res.status(err.status || 500)
